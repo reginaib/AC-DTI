7b869c0804d05275185b533da0343f9b944c0683
reginaib <regina.r.ibragimova@gmail.com>
2024-03-03 22:31:25 +0100

    added a target and metrics

--- scripts/DDC_wt.py
+++ scripts/DDC_wt.py
@@ -12,6 +12,8 @@ from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
 from pandas import read_csv
 from itertools import chain
 from pickle import load, dump
+from torchmetrics.classification import BinaryRecall, BinaryAccuracy, BinaryF1Score, BinaryPrecision
+from torchmetrics import MetricCollection
 
 
 class DrugDrugCliffNN(LightningModule):
@@ -26,20 +28,17 @@ class DrugDrugCliffNN(LightningModule):
             nn.ReLU()
         )
 
-        self.t_encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim_t),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(hidden_dim_t, hidden_dim_t // 2),
-            nn.ReLU()
-        )
+        self.t_encoder = nn.Embedding(230, 128)
 
         self.classifier = nn.Sequential(
-            nn.Linear(hidden_dim_d // 2 + hidden_dim_t // 2, hidden_dim_c),
+            nn.Linear(hidden_dim_d + hidden_dim_t, hidden_dim_c),
             nn.ReLU(),
             nn.Dropout(0.1),
             nn.Linear(hidden_dim_c, 1)
         )
+
+        self.metrics = MetricCollection([BinaryRecall(), BinaryAccuracy(), BinaryF1Score(), BinaryPrecision()],
+                                        prefix='test/')
 
     def forward(self, drug1, drug2, target):
         # Process each compound through the same branch
@@ -54,26 +53,31 @@ class DrugDrugCliffNN(LightningModule):
         return self.classifier(combined_out).flatten()
 
     def training_step(self, batch):
-        drug1, drug2, clf = batch
+        drug1, drug2, clf, target = batch
 
-        preds = self(drug1, drug2)
+        preds = self(drug1, drug2, target)
         ls = F.binary_cross_entropy_with_logits(preds, clf)
         self.log('Training/BCELoss', ls)
         return ls
 
     def validation_step(self, batch, _):
-        drug1, drug2, clf = batch
+        drug1, drug2, clf, target = batch
 
-        preds = self(drug1, drug2)
+        preds = self(drug1, drug2, target)
         ls = F.binary_cross_entropy_with_logits(preds, clf)
         self.log('Validation/BCELoss', ls)
 
     def test_step(self, batch, *_):
-        drug1, drug2, clf = batch
+        drug1, drug2, clf, target = batch
 
-        preds = self(drug1, drug2)
+        preds = self(drug1, drug2, target)
         ls = F.binary_cross_entropy_with_logits(preds, clf)
+        self.metrics.update(preds.sigmoid(), clf)
         self.log('Test/BCELoss', ls)
+
+    def on_test_epoch_end(self):
+        self.log_dict(self.metrics.compute())
+        self.metrics.reset()
 
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=1e-4)
@@ -90,7 +94,7 @@ class DrugDrugData(LightningDataModule):
         self.batch_size = batch_size
 
     def prepare_data(self):
-        # expected next columns: smiles1, smiles2, cliff, split
+        # expected next columns: smiles1, smiles2, cliff, split, target
         data = read_csv(self.csv)
 
         cache = {}
@@ -110,20 +114,21 @@ class DrugDrugData(LightningDataModule):
         drugs1 = torch.stack(drugs1)
         drugs2 = torch.stack(drugs2)
         cliff = torch.tensor(data.cliff, dtype=torch.float32)
-        split = torch.tensor(data.split, dtype=torch.int8)
+        split = torch.tensor(data.split, dtype=torch.long)
+        target = torch.tensor(data.target, dtype=torch.long)
         with open(self.cache, 'wb') as f:
-            dump((drugs1, drugs2, cliff, split), f)
+            dump((drugs1, drugs2, cliff, split, target), f)
 
     def setup(self, stage=None):
         with open(self.cache, 'rb') as f:
-            drugs1, drugs2, cliff, split = load(f)
+            drugs1, drugs2, cliff, split, target = load(f)
 
         mask = split == 0
-        self._train = TensorDataset(drugs1[mask], drugs2[mask], cliff[mask])
+        self._train = TensorDataset(drugs1[mask], drugs2[mask], cliff[mask], target[mask])
         mask = split == 1
-        self._validation = TensorDataset(drugs1[mask], drugs2[mask], cliff[mask])
+        self._validation = TensorDataset(drugs1[mask], drugs2[mask], cliff[mask], target[mask])
         mask = split == 2
-        self._test = TensorDataset(drugs1[mask], drugs2[mask], cliff[mask])
+        self._test = TensorDataset(drugs1[mask], drugs2[mask], cliff[mask], target[mask])
 
     def train_dataloader(self):
         return DataLoader(self._train, batch_size=self.batch_size, shuffle=True)
