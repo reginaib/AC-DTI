7471b13ebf37252dc853e13b8e71ebee6b6c1a80
reginaib <regina.r.ibragimova@gmail.com>
2024-04-05 22:34:25 +0200

    added pre_trained_d_encoder_path

--- scripts/DTI_sweep.py
+++ scripts/DTI_sweep.py
@@ -9,20 +9,19 @@ sweep_config = {
         },
         "parameters": {
             "mode": {"value": "DTI"},
-            "n_hidden_layers": {"values": [1, 2, 3, 4]},
-            "hidden_dim_d": {"values": [32, 64, 128, 256, 512, 756, 1024]},
+            "n_hidden_layers": {"value": [1]},
+            "hidden_dim_d": {"values": [1024]},
             "hidden_dim_t": {"values": [32, 64, 128, 256, 512, 756, 1024]},
             "hidden_dim_c": {"values": [32, 64, 128, 256, 512, 756, 1024]},
             "lr": {"values": [0.00001, 0.00003, 0.0001, 0.0003, 0.001]},
             "dr": {'values': [0.01, 0.1, 0.2, 0.3, 0.4, 0.5]},
-            # 'lr': {'max': 0.001, 'min': 0.00001},
-            # 'dr': {'max': 0.5, 'min': 0.01},
             "patience": {"value": 15},
             "accelerator": {"value": "gpu"},
             "max_epochs": {"value": 100},
-            "dataset_name": {"value": 'data/kiba_d_t_aff_smiles_split.csv'},
+            "dataset_name": {"value": './analysis/kiba_d_t_aff_smiles_split.csv'},
+            "pre_trained_d_encoder_path": {"value": None},
         }
 }
 
 
-start_sweep(config=sweep_config, project_name='DTI_aff_sweep', num_config=50)
+start_sweep(config=sweep_config, project_name='DTI_sweep', num_config=100)
