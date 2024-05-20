9de6c8f6b3a92ee7df8cedec49ed14737cab1aad
reginaib <regina.r.ibragimova@gmail.com>
2024-05-20 22:07:03 +0200

    added checkpoint

--- scripts/DDC_wt.py
+++ scripts/DDC_wt.py
@@ -2,20 +2,20 @@ from finetuning import start_training
 
 
 class config:
-
     n_hidden_layers = 1
-    n_targets = 676
-    hidden_dim_d = 1024
-    hidden_dim_t = 756
-    hidden_dim_c = 756
-    lr = 0.0003
-    dr = 0.01
+    n_targets = 666
+    hidden_dim_d = 256
+    hidden_dim_t = 128
+    hidden_dim_c = 1024
+    lr = 0.001
+    dr = 0.2
     patience = 15
     accelerator = 'gpu'
-    max_epochs = 100
-    dataset_name = '../analysis/bindingdb_ki_cliff_pairs_ta1_ts0.9_r_wt.csv'
+    max_epochs = 5
+    dataset_name = './analysis/acnet_cliff_pairs_r_wt.csv'
     save_preds = False
+    checkpoint = False
 
 
-start_training(mode='DDC', config=config, project_name='DDC_r_best')
+start_training(mode='DDC', config=config, project_name='DDC_ACNet_tryout')
 
