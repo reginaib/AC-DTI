85004b75ffb472f5d16c2f5bea1cb6b06171ac9b
reginaib <regina.r.ibragimova@gmail.com>
2024-11-26 15:46:47 +0100

    fixed config

--- scripts/DTI.py
+++ scripts/DTI.py
@@ -4,19 +4,21 @@ from finetuning import start_training
 # Define a configuration class with various hyperparameters and settings for training
 class config:
     n_hidden_layers = 1
-    n_targets = 676
-    hidden_dim_d = 256
-    hidden_dim_t = 256
-    hidden_dim_c = 1024
-    lr = 0.0001
-    dr = 0.01
+    n_targets = 229
+    hidden_dim_d = 768
+    hidden_dim_t = 768
+    hidden_dim_c = 512
+    lr = 0.001
+    dr = 0.1
     patience = 15
     accelerator = 'gpu'
     max_epochs = 100
-    dataset_name = './analysis/kiba_d_t_aff_smiles_split.csv'
-    # pre_trained_d_encoder_path = './results/DDC_KIBA_best_epoch_23.ckpt'
+    dataset_name = './analysis/kiba_dti_cb_split.csv'
+    # pre_trained_d_encoder_path = './results/DDC_best_epoch_23.ckpt'
     pre_trained_d_encoder_path = None
-    preds_name = 'DTI_vary_all_train_best_1404'
+    # pre_trained_t_encoder_path = './results/DDC_best_epoch_23.ckpt'
+    pre_trained_t_encoder_path = None
+    preds_name = 'DTI_KIBA_cb_bl_best_preds_3'
     freeze = False
     layer_to_d_encoder = False
     hidden_dim_d_add = 0
@@ -30,4 +32,4 @@ class config:
 # - mode: The mode of operation ('DTI')
 # - config: The configuration class containing all the hyperparameters and settings
 # - project_name: The name of the project in Weights and Biases where the training results will be logged
-start_training(mode='DTI', config=config, project_name='DTI_vary_all_best_train')
+start_training(mode='DTI', config=config, project_name='DTI_KIBA_cb_bl_best_train')
